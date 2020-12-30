93c3ebfd7d0c662ca46793f90283ef975da88f50
NaokiM03 <mattsun.phone@gmail.com>
2020-12-30 17:17:45 +0900

    feat: compile number

--- cc/src/cc.nim
+++ cc/src/cc.nim
@@ -1,5 +1,13 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import system
+import os
 
 when isMainModule:
-  echo("Hello, World!")
+  if paramCount() != 1:
+    quit("invalid number of arguments\n", 1)
+
+  echo "  .globl main"
+  echo "main:"
+  echo "  mov $", $commandLineParams()[0], ", %rax"
+  echo "  ret"
+
+  quit(0)
