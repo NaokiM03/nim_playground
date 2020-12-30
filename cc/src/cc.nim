789fe58a1f1970ddefa790afad08590c35f85b31
NaokiM03 <mattsun.phone@gmail.com>
2020-12-31 04:38:38 +0900

    feat: utils

--- cc/src/cc.nim
+++ cc/src/cc.nim
@@ -1,6 +1,19 @@
 import system
 import os
 
+proc inc(i: var int, n = 1) =
+  i = i + n
+
+proc firstNumStr(str: string): string =
+  var s = ""
+  var i = 0
+  while str[i].isDigit:
+    s.add(str[i])
+    i.inc
+    if i >= str.len:
+      break
+  return s
+
 when isMainModule:
   if paramCount() != 1:
     quit("invalid number of arguments\n", 1)
